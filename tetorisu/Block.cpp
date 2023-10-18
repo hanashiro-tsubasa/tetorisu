f9b1badc7f80d2bb595462c0b49d7db4aa43e65d
hanashiro-tsubasa <138739148+hanashiro-tsubasa@users.noreply.github.com>
2023-10-18 15:00:00 +0900

    プログラムの追加
    プログラムを追加しました

---
+++ tetorisu/Block.cpp
@@ -0,0 +1,34 @@
+#include"Block.h"
+#include"DxLib.h"
+#include"InputControl.h"
+
+/*************************************************
+*É}ÉNÉçíËã`
+* ***********************************************/
+#define FIELD_HEIGHT
+#define FIELD_WIDTH
+#define BLOCK_TROUT_SIZE
+#define BLOCK_SIZE
+#define BLOCK_TYPE_MAX
+#define BLOCK_NEXT_POS_X
+#define BLOCK_NEXT_POS_Y
+#define BLOCK_STOCK_POS_X 
+#define BLOCK_STOCK_POS_Y
+#define DROP_BLOCK_INIT_X
+#define DORP_BLOCK_INIT_Y
+#define TURN_
+#define
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tetorisu/RankingScene.cpp
+++ tetorisu/RankingScene.cpp
@@ -325,7 +325,11 @@ void ranking_input_name_draw(void)
 	for (i = 0; i < 26; i++)
 	{
 		//8
+
 		DrawFormatString((i % 13 * 50) + 300, (i / 13 * 50) + 330, GetColor(255, 255,
+			255), "%-3c", 'a' + i);
+
+		DrawFormatString((i % 13 * 50) + 300, (i / 13 * 50) + 430, GetColor(255, 255,
 			255), "%-3c", 'A' + i);
 	}
 	for (i = 0; i < 10; i++)
